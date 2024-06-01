96edfbf919b5418a8c0fa6e75c364711013a90ad
natalyachaadaeva <89857792+natalyachaadaeva@users.noreply.github.com>
2024-06-01 22:18:16 +0300

    Изменили текст в Java

--- src/MyFirstApp.java
+++ src/MyFirstApp.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class MyFirstApp {
     public static void main(String[] args) {
-        System.out.println("Напиgit coсано однажды, работает везде");
+        System.out.println("Написано однажды, работает везде");
         Scanner console = new Scanner(System.in);
         System.out.print("Введите свое имя: ");
         System.out.println(console.nextLine());
