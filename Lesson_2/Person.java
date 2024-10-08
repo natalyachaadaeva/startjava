423f3a02bec357ff74b76f209cea74cd713ea367
natalyachaadaeva <natalia.slinko@gmail.com>
2024-10-08 22:45:49 +0300

    Домашнее задание на создание класса Person

---
+++ Lesson_2/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    String gender = "M";
+    String name = "Иван";
+    float height = 1,80f;
+    float weight = 80f;
+    int age = 34;
+
+    void move() {
+        System out print("Идет");
+    }
+
+    void run() {
+        System out print("Бежит");
+    }
+
+    String speak() {
+        System out print("Говорит");
+    }
+
+    void learnJava() {
+        System out print("Учит Java");
+    }
+}
