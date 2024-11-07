5f8ed76aa192c710f9dd861d1a23dbf1d21025d0
natalyachaadaeva <natalia.slinko@gmail.com>
2024-11-07 22:43:50 +0300

    Добавлены Jaeger и JaegerTest

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,58 @@
+public class Jaeger {
+    private String modelName = "Striker Eureka";
+    private float height = 2.3f;
+    private float weight = 5.5f;
+    private int armor = 9;
+
+    public Jaeger(String modelName, float height, float weight, int armor) {
+        this.modelName = modelName;
+        this.height = height;
+        this.weight = weight;
+        this.armor = armor;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public void move() {
+        System.out.println("Вы прошли 5 шагов");
+    }
+
+    public String scanKaiju() {
+        return "nothing";
+    }
+
+    public boolean drift() {
+        System.out.print("Вы вошли в дрифт");
+        return true;
+    }
+}
