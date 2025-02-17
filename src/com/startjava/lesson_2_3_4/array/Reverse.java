1212fbf5a1a7298a4d014765c90dea5d675f287a
natalyachaadaeva <natalia.slinko@gmail.com>
2025-02-17 22:49:34 +0300

    восстановление файлов

--- src/com/startjava/lesson_2_3_4/array/Reverse.java
+++ src/com/startjava/lesson_2_3_4/array/Reverse.java
@@ -3,40 +3,55 @@ package com.startjava.lesson_2_3_4.array;
 import java.util.Arrays;
 
 public class Reverse {
-    public static long[] array(int[] arr) {
+    public static long[] getArray(int[] arr) {
         if (arr == null) {
-            System.out.println("Array is null");
+            System.out.println("   Array is null");
             return null;
         }
-        System.out.print("До реверса: [");
+        System.out.printf("%6s", "   До реверса: [");
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
             if (i < arr.length - 1) {
                 System.out.print(", ");
             }
         }
-        System.out.println("]");
+        System.out.print("]");
+        System.out.println();
+        return null;
+    }
 
-        Arrays.sort(arr);
-
+    public static long[] reverseArray(int[] arr) {
+        if (arr == null) {
+            System.out.println("   Array is null");
+            return null;
+        }
         System.out.print("После реверса: [");
+        Arrays.sort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
             if (i < arr.length - 1) {
                 System.out.print(", ");
             }
         }
-        System.out.println("]");
-
+        System.out.print("]");
         System.out.println();
         return null;
     }
 
     public static void main(String[] args) {
-        array(new int[0]); // массив нулевой длины
-        array(null); // null приводит к ошибке
-        array(new int[]{6, 8, 9, 1});
-        array(new int[]{13, 8, 5, 3, 2, 1, 1}); // 13, 8, 5, 3, 2, 1, 1
+        getArray(new int[0]); // массив нулевой длины
+        reverseArray(new int[0]); // массив нулевой длины
+
+        getArray(null); // null приводит к ошибке
+        reverseArray(null); // null приводит к ошибке
+
+        getArray(new int[]{6, 8, 9, 1});
+        reverseArray(new int[]{6, 8, 9, 1});
+
+        getArray(new int[]{13, 8, 5, 3, 2, 1, 1}); // 13, 8, 5, 3, 2, 1, 1
+        reverseArray(new int[]{13, 8, 5, 3, 2, 1, 1}); // 13, 8, 5, 3, 2, 1, 1
         System.out.println();
+
+
     }
 }
